8820e1139bb29a31c3af7209182ee50e74040be3
Mariam Jaludi <mariam.jaludi@gmail.com>
2018-11-14 20:04:07 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -6,16 +6,17 @@ function forLoop(array) {
 }
 
 function whileLoop(n){
-
-  while (n != 0){
+  while (n > 0){
     console.log(n);
     n--;
   }
+  return 1;
 }
 
 function doWhileLoop(array){
   do {
     array.pop();
   }
-  while (array.length() != 0)
+  while (array.length > 0)
+  return array;
 }
